63440e529a53f53deeeb860c495ba549276c9a9c
Alistair Willis <alistairrwillis@gmail.com>
2017-05-08 13:11:45 +0900

    update

--- ChatCAT/app/socket/index.js
+++ ChatCAT/app/socket/index.js
@@ -4,8 +4,6 @@ const h = require('../helpers');
 module.exports = (io, app) => {
   let allrooms = app.locals.chatrooms;
 
-  
-
   // listen to roomslist pipeline on sockets connection
   io.of('/roomslist').on('connection', socket => {
     console.log('Socket.io connected to client'); 
@@ -31,17 +29,25 @@ module.exports = (io, app) => {
     });
   }); 
 
+  // Join a chatroom
   io.of('/chatter').on('connection', socket => {
     socket.on('join', data => {
       let usersList = h.addUserToRoom(allrooms, data, socket); 
-      console.log('chatter', usersList); 
+       
       socket.broadcast.to(data.roomID).emit('updateUsersList', JSON.stringify(usersList.users)); 
       socket.emit('updateUsersList', JSON.stringify(usersList.users)); 
     });
 
+    // When a socket exits
     socket.on('disconnect', () => {
       let room = h.removeUserFromRoom(allrooms, socket); 
       socket.broadcast.to(room.roomID).emit('updateUsersList', JSON.stringify(room.users)); 
-    })
+    }); 
+
+    //When a new message arrives
+    socket.on('newMessage', data => {
+      // socket.to === socket.broadcast.to
+      socket.to(data.roomID).emit('inMessage', JSON.stringify(data));
+    });
   }); 
 }
