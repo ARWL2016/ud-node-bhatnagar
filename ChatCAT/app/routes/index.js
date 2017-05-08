f5500f079f5f4d577ab6f91e106ce03aecd0a793
Alistair Willis <alistairrwillis@gmail.com>
2017-05-08 21:32:00 +0900

    update

--- ChatCAT/app/routes/index.js
+++ ChatCAT/app/routes/index.js
@@ -34,11 +34,11 @@ module.exports = () => {
         successRedirect: '/rooms', 
         failureRedirect: '/'
       }), 
-      '/auth/twitter': passport.authenticate('twitter'), 
-      '/auth/twitter/callback': passport.authenticate('twitter', {
-        successRedirect: '/rooms', 
-        failureRedirect: '/'
-      }), 
+      // '/auth/twitter': passport.authenticate('twitter'), 
+      // '/auth/twitter/callback': passport.authenticate('twitter', {
+      //   successRedirect: '/rooms', 
+      //   failureRedirect: '/'
+      // }), 
       '/logout': (req, res, next) => {
         // passport method which ends a session
         req.logout(); 
