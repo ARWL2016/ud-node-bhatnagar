a65899aa7d9ba6cdfe8ebd3976e8ce042538ac5a
Alistair Willis <alistairrwillis@gmail.com>
2017-05-08 15:24:10 +0900

    add redis

--- ChatCAT/app/index.js
+++ ChatCAT/app/index.js
@@ -1,5 +1,9 @@
 'use strict'; 
 
+const config = require('./config'); 
+const redis = require('redis').createClient; 
+const adapter = require('socket.io-redis'); 
+
 // OAuth Logic 
 require('./auth')(); 
 
@@ -8,6 +12,20 @@ let ioServer = (app) => {
   app.locals.chatrooms = []; // stored in memory
   const server = require('http').Server(app); 
   const io = require('socket.io')(server);
+  // configure sockets to only use websockets (not long polling)
+  io.set('transports', ['websocket']); 
+  let pubClient = redis(config.redis.port, config.redis.host, {
+    auth_pass: config.redis.password
+  });
+  // get data from redis 
+  let subClient = redis(config.redis.port, config.redis.host, {
+    return_buffers: true, 
+    auth_pass: config.redis.password
+  }); 
+  io.adapter(adapter({
+    pubClient, 
+    subClient
+  }));
   // get the request data from the express session
   io.use((socket, next) => {
     require('./session')(socket.request, socket.request.res, next); 
